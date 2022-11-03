21e1f2e5e9be7d2aea3c5abe426002c4a2ef8108
songyuankun <songyuankun@didiglobal.com>
2022-11-03 17:25:39 +0800

    add jdSDK
    Change-Id: Ifdb399907810667e847d6798da61a7043f683fda

--- src/main/java/com/songyuankun/jd/UnionJdProxy.java
+++ src/main/java/com/songyuankun/jd/UnionJdProxy.java
@@ -40,6 +40,8 @@ public class UnionJdProxy {
     private String secretKey;
     @Value("${jd.site_id}")
     private Long siteId;
+    @Value("${jd.web_cookie}")
+    private String webCookie;
 
     public String getCommand(final String skuUrl, String positionId) {
 
@@ -159,7 +161,7 @@ public class UnionJdProxy {
         jsonObject.put("body", body);
         HttpRequest httpRequest = HttpUtil.createGet(url)
                 .form(jsonObject)
-                .header(Header.COOKIE, "shshshfpb=cAYXTTeRd83x8SbRDRU-C5A; shshshfpa=b50522f7-1351-f8f1-663a-ba1debf5795c-1653792131; whwswswws=; warehistory=\"10056670674874,10056670674874,10056670674874,10056670674874,10056670674874,10056670674874,10056670674874,10050064328710,100022193732,100001621535,100014384352,\"; autoOpenApp_downCloseDate_jd_homePage=1665140250682_1; jcap_dvzw_fp=tVLtMZvmNwN96JdfzFSRAGA-IIDwY6FawHPMxnC_ky_PFaGjfJlEhcfJA8YLcIwEAq4xYQ==; __jdv=76161171|direct|-|none|-|1666416713302; __jdu=16664167133011822377983; areaId=1; ipLoc-djd=1-2901-0-0; PCSYCityID=CN_110000_110100_110114; shshshfp=c10d629c39a7673012cfe47cf00f22eb; user-key=0e0c1b0c-68df-4d14-b249-bfa54eb25bda; 3AB9D23F7A4B3C9B=RJKW6NTITPSEIDL7TQZHRNFL3FS5XU2BQIDCBOO2BCSW7ASLI3XUBFYC6RKVJH4P3JPRTWZBA7TW2OQIFP7YXHZECA; TrackID=13-R3ku1X-SP3sLsX7Nz0C6Ttet5cKC05cSgpDOScRjknU1WRQw9K-5Os92sr3M3tR9qHOITSwtzci9dfS0hnsytsMROJdy_wvgbvXKu5y0A; pinId=lyNEjbpwGPQJ3BsDd-BBl7V9-x-f3wj7; pin=jd_5861e418610ca; unick=jd_182011lyr; ceshi3.com=203; _tp=Lqz99JyKiqlWLWkf9WpF9j4uwwduOKTO26OwS3pC9Iw%3D; _pst=jd_5861e418610ca; __jdc=209449046; __jda=209449046.16664167133011822377983.1666416713.1666491011.1666499161.3; thor=0B5674FC93F071F28C986463AF01DE42F5794F2A312B1D47FC79BFD045D9E7C9E4C4914EB96B883F9204B9BB811BCA703E0EE085856E1283379CA3F86E016E1175D33EF269BDF8865BDE95EA4DF7F65CC7DB9CE55C93191F6CC00C09A629EDBC71AEBC06AAB3937A50E745EB47920127D5F5F442AC5E010C498056C4B4F0C7999E9ADCA5B3466A54247F7B657FCFAC2B018C2101365F744B7296BCE147BC4F63; __jdb=209449046.27.16664167133011822377983|3.1666499161; RT=\"z=1&dm=jd.com&si=6jepue0pqdh&ss=l9kv0sdf&sl=j&tt=1cj&ld=e2kl&nu=e40add4e0e00b1080626db93d2f6e517&cl=e02d\"");
+                .header(Header.COOKIE, webCookie);
         try (HttpResponse execute = httpRequest.execute()) {
             return JSONObject.parseObject(execute.body());
         }
