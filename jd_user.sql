43b189d23a5f5fddccf49299062550ac4c85e0f6
songyuankun <123839070@qq.com>
2022-12-22 00:15:33 +0800

    back up sql

---
+++ jd_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO wechat.jd_user (id, position_id, wechat_user) VALUES (1, '3100129815', 'ohRwUwLzpfmIGrqmSV281oEL1qu8');
+INSERT INTO wechat.jd_user (id, position_id, wechat_user) VALUES (2, '3100130134', 'ohRwUwGsbjOw66K2O0vMZbM07G4Q');
+INSERT INTO wechat.jd_user (id, position_id, wechat_user) VALUES (3, '3100139794', 'ohRwUwAGBLQiOrSVoPEh-QRv7uuc');
