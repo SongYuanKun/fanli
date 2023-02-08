aaa961f6f0fd7ea72fcce6808a2c4d0e491e915e
songyuankun <songyuankun@didiglobal.com>
2023-02-08 14:19:12 +0800

    fix error

--- src/main/java/com/songyuankun/taobao/UnionTaoBaoService.java
+++ src/main/java/com/songyuankun/taobao/UnionTaoBaoService.java
@@ -2,6 +2,7 @@ package com.songyuankun.taobao;
 
 import com.songyuankun.unionService;
 import com.taobao.api.response.TbkDgMaterialOptionalResponse;
+
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
@@ -26,23 +27,23 @@ public class UnionTaoBaoService implements unionService {
     public String getGoodInfo(String url, String fromUserId) {
         List<TbkDgMaterialOptionalResponse.MapData> mapDataList = unionTaoBaoProxy.getCommand(url);
         if (CollectionUtils.isEmpty(mapDataList)) {
-            return "该商品不参与优惠";
+            return null;
         }
         TbkDgMaterialOptionalResponse.MapData mapData = mapDataList.get(0);
         return "商品名称：" + mapData.getTitle() + "\r\n" +
-                "价格：" + mapData.getReservePrice() + "\r\n" +
-                "返佣比例：" + mapData.getCommissionRate() + "‰\r\n" +
-                "预计返佣：" +
-                new BigDecimal(mapData.getReservePrice())
-                        .multiply(new BigDecimal(mapData.getCommissionRate()))
-                        .multiply(new BigDecimal("0.001"))
-                        .setScale(2, RoundingMode.UP)
-                + "\r\n" +
-                "下单地址：https:" +
-                StringUtils.defaultIfBlank(
-                        mapData.getCouponShareUrl(),
-                        mapData.getUrl()
-                ) +
-                "";
+            "价格：" + mapData.getReservePrice() + "\r\n" +
+            "返佣比例：" + mapData.getCommissionRate() + "‰\r\n" +
+            "预计返佣：" +
+            new BigDecimal(mapData.getReservePrice())
+                .multiply(new BigDecimal(mapData.getCommissionRate()))
+                .multiply(new BigDecimal("0.001"))
+                .setScale(2, RoundingMode.UP)
+            + "\r\n" +
+            "下单地址：https:" +
+            StringUtils.defaultIfBlank(
+                mapData.getCouponShareUrl(),
+                mapData.getUrl()
+            ) +
+            "";
     }
 }
